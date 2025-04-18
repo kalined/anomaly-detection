da57fa57fed1a99527649f77c57601d9045d6c10
Edgar Kalinovski <D000036@danskenet.net>
2025-04-18 19:24:34 +0300

    Add testing part, random forest module

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
-import anomaly_detection.data_preprocessing
+import joblib
 
-anomaly_detection.data_preprocessing
+from anomaly_detection.models import random_forest
+
+def main():
+    print("Starting the random forest module from main.py...")
+    random_forest.main()
+
+    #model = joblib.load("./models/random_forest_model.pkl")
+
+    #sample = [[15, 15, 0.4014,0.0,492508.0,0.051,4.94,5.35,4.8,0.5005,383445.0,92989.0,0.0,4.2,0.0,0.0,0.0,0.0]]
+
+    #scaler = joblib.load("./models/random_forest_scaler.pkl")
+    #sample_scaled = scaler.transform(sample)
+
+    #predicted_class = model.predict(sample_scaled)
+    #label_map = {
+    #0: "normal",
+    #1: "user_peak",
+    #2: "ddos",
+    #3: "cpu_load_anomaly",
+    #4: "server_unavailability",
+    #5: "memory_leak"
+    #}
+
+    #print("Predicted label:", label_map[int(predicted_class[0])])
+
+
+if __name__ == '__main__':
+    main()
+
